5d2123263d18fa2f4378dc7176044206cc137b95
15719026 <>
2021-05-23 16:36:12 +0000

    TMIN

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -44,6 +44,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
+tmin_nodata_count = data.iloc[:,8].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -56,6 +57,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
